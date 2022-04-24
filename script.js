d43d683f7917592d689ee022aaf0d2497da78186
matteoorig <matteoorig@gmail.com>
2022-04-24 12:54:00 +0200

    update

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ const root = document.querySelector(":root");
 
 if (vW <= 450 && vH <= 1000) {
   //mobile
+  screen.orientation.lock("portrait");
 } else {
   //Desktop
   window.addEventListener("mousemove", (ev) => (mouse = getMousePos(ev)));
