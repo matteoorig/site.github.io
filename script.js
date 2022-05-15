687da0c8a86685dd5039f327dcdd0f2f64f0e7ef
matteoorig <matteoorig@gmail.com>
2022-05-15 11:11:04 +0200

    update

--- script.js
+++ script.js
@@ -191,61 +191,6 @@ if (vW <= 450 && vH <= 1000) {
     });
   });
 
-  //item animation
-  objcentral.addEventListener("mouseenter", ()=>{
-    
-    gsap.to(objcentral, {
-      duration: 0.6,
-      width: "105%",
-      xPercent: -15,
-      ease: "Power3.easeOut",
-    });
-  })
-  objcentral.addEventListener("mouseleave", ()=>{
-    
-    gsap.to(objcentral, {
-      duration: 0.6,
-      width: "95%",
-      xPercent: 0,
-      ease: "Power3.easeOut",
-    });
-  })
-  objtop.addEventListener("mouseenter", ()=>{
-    
-    gsap.to(objtop, {
-      duration: 0.6,
-      width: "20%",
-      xPercent: -15,
-      ease: "Power3.easeOut",
-    });
-  })
-  objtop.addEventListener("mouseleave", ()=>{
-    
-    gsap.to(objtop, {
-      duration: 0.6,
-      width: "15%",
-      xPercent: 0,
-      ease: "Power3.easeOut",
-    });
-  })
-  objbottom.addEventListener("mouseenter", ()=>{
-    
-    gsap.to(objbottom, {
-      duration: 0.6,
-      width: "20%",
-      xPercent: -15,
-      ease: "Power3.easeOut",
-    });
-  })
-  objbottom.addEventListener("mouseleave", ()=>{
-    
-    gsap.to(objbottom, {
-      duration: 0.6,
-      width: "15%",
-      xPercent: 0,
-      ease: "Power3.easeOut",
-    });
-  })
 
   //on hover horizontal scroll
   hs_area.forEach((obj) => {
